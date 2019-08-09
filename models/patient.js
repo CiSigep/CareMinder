0c208bdd7a02bd2875ac2b4456509301d7cdc841
CiSigEp <cisigep@gmail.com>
2019-08-09 11:31:38 -0500

    Create database structure, remove extra config lines in config.json

---
+++ models/patient.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+  // Define a patient table with these properties.
+  var Patient = sequelize.define("Patient", {
+    age: DataTypes.INTEGER,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING
+  });
+
+  // Associate the patient table to belong to the caregiver and have
+  // many tasks and bills.
+  Patient.associate = function(models) {
+    Patient.belongsTo(models.Caregiver, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+    Patient.hasMany(models.Task);
+    Patient.hasMany(models.Bill);
+  };
+
+  return Patient;
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,24 +3,33 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    console.log(db);
+
+    res.json({ data: true });
+    /* db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
-    });
+    }); */
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    console.log(db);
+
+    res.json({ data: true });
+    /* db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
-    });
+    }); */
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+    console.log(db);
+
+    res.json({ data: true });
+    /* db.Example.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.json(dbExample);
-    });
+    }); */
   });
 };
