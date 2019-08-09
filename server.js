42aa4a3599d4142851e4cea656edbcb533f8cacb
Joe P <joe.peterson2729@gmail.com>
2019-08-08 20:17:31 -0500

    changed routes folder name and added controller file

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./controllers/apiRoutes")(app);
+require("./controllers/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
