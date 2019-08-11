4a1474c0b1a0b471c5019bb67e2e8a375aaf9213
Joe P <joe.peterson2729@gmail.com>
2019-08-11 12:33:38 -0500

    patient controller with partial paths

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -2,31 +2,35 @@ var express = require("express");
 var careDAO = require("../DAO/careDAO");
 var router = express.Router();
 
-router.get("/api/patient/:id", function(req, res) {
-  res.json({ data: true });
+router.get("/api/patient/:id", function (req, res) {
+    res.json({ data: true });
 });
 
-router.get("/", function(req, res) {
-  res.json({ data: true });
+router.get("/", function (req, res) {
+    res.json({ data: true });
 });
 
-router.get("/api/pages??", function(req, res) {
-  res.json({ data: true });
+router.get("/api/pages??", function (req, res) {
+    res.json({ data: true });
 });
 
-router.post("api/task", function(req, res) {
-  res.json({ data: true });
+router.post("api/task", function (req, res) {
+    res.json({ data: true });
 });
-router.post("api/bill", function(req, res) {
-  res.json({ data: true });
+
+router.post("api/bill", function (req, res) {
+    res.json({ data: true });
 });
-router.delete("/api/task/:id", function(req, res) {
-   res.json({data: true});
-  
-    router.delete("/api/bill/:id", function(req, res) {
-       res.json({data: true});
-      
-       patient.deleteOne(condition, function(result) {
+
+router.delete("/api/task/:id", function (req, res) {
+    res.json({ data: true });
+});
+
+router.delete("/api/bill/:id", function (req, res) {
+    res.json({data: true});
+});
+
+    patient.deleteOne(condition, function (result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
@@ -35,5 +39,5 @@ router.delete("/api/task/:id", function(req, res) {
         }
     });
 });
-   
+
 module.exports = router;
