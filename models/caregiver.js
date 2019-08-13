f7a421baf84e70fdcc379064107f5bb308b688d6
CiSigEp <cisigep@gmail.com>
2019-08-12 19:30:58 -0500

    Make username unque

--- models/caregiver.js
+++ models/caregiver.js
@@ -11,7 +11,8 @@ module.exports = function(sequelize, DataTypes) {
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
